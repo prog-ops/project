f2f074b3b6341dfa8bb56959789e294bb30f2522
prgrm274 <programmer270487@gmail.com>
2025-02-04 23:34:32 +0700

    Add basic PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,40 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        tailwindcss()
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'React PWA',
+                short_name: 'PWA',
+                description: 'A modern searchable React dropdown component',
+                theme_color: '#ffffff',
+                background_color: '#959d7b',
+                start_url: '/',
+                scope: '/',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: '/icons/pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/pwa-512x512.png',
+                        type: 'image/png',
+                        sizes: '512x512'
+                    }
+                ],
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            }
+        })
     ],
 })
